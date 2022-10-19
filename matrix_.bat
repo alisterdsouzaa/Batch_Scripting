55db2f97d4d527445507604d34fff891e49bdc73
Alister <alisterdsouzaa@outlook.com>
2022-10-19 11:34:26 +0530

    Add files via upload

---
+++ matrix_.bat
@@ -0,0 +1,17 @@
+@echo off
+
+rem +-------------------------------------------------------------+
+rem |    Author : Alister D'souza                                 |
+rem |    Date   : 19/10/2022                                      |
+rem |    Contact: @alisterdsouzaa                                 |
+rem |    Script : falling matrix        					  |
+rem +-------------------------------------------------------------+
+
+:: color to Red
+color 0c
+
+goto a
+:a
+
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto a
